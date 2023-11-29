08a8a404dbf8aee4333efb81dbc0c730b278366e
Fuzail Raza <fuzailraza161@gmail.com>
2023-11-30 01:02:32 +0500

    Added SMS Module

---
+++ mongoConnect/com/infobip/sms/SendSMS.java
@@ -0,0 +1,51 @@
+
+
+package com.infobip.sms;
+
+import com.infobip.ApiClient;
+import com.infobip.ApiException;
+import com.infobip.ApiKey;
+import com.infobip.BaseUrl;
+import com.infobip.api.SmsApi;
+import com.infobip.model.SmsAdvancedTextualRequest;
+import com.infobip.model.SmsDestination;
+import com.infobip.model.SmsTextualMessage;
+
+import java.util.Collections;
+
+public class SendSMS {
+
+    private static final String BASE_URL = "https://e1dlgq.api.infobip.com";
+    private static final String API_KEY = "b40faa0d2e7a08ce4eb41067b589e1e6-e4685a16-26a5-420a-b412-12324caafb6f";
+    private static final String RECIPIENT = "923014384681";
+
+    public static void main(String[] args) {
+        // Create the API client and the Send SMS API instances.
+        ApiClient apiClient = ApiClient.forApiKey(ApiKey.from(API_KEY))
+                .withBaseUrl(BaseUrl.from(BASE_URL))
+                .build();
+        SmsApi sendSmsApi = new SmsApi(apiClient);
+
+        // Create a message to send.
+        SmsTextualMessage smsMessage = new SmsTextualMessage()
+                .addDestinationsItem(new SmsDestination().to(RECIPIENT))
+                .text("This message is for API Testing");
+
+        // Create a send message request.
+        SmsAdvancedTextualRequest smsMessageRequest = new SmsAdvancedTextualRequest()
+                .messages(Collections.singletonList(smsMessage));
+
+        try {
+            // Send the message.
+            com.infobip.model.SmsResponse smsResponse = sendSmsApi.sendSmsMessage(smsMessageRequest).execute();
+            System.out.println("Response body: " + smsResponse);
+
+            // Get delivery reports. It may take a few seconds to show the above-sent message.
+            com.infobip.model.SmsDeliveryResult reportsResponse = sendSmsApi.getOutboundSmsMessageDeliveryReports().execute();
+            System.out.println(reportsResponse.getResults());
+        } catch (ApiException e) {
+            System.out.println("HTTP status code: " + e.responseStatusCode());
+            System.out.println("Response body: " + e.rawResponseBody());
+        }
+    }
+}
--- mongoConnect/test.java
+++ mongoConnect/test.java
@@ -1,69 +1,13 @@
-import javax.swing.*;
-import java.awt.*;
+import java.sql.*;
 
-public class test extends JFrame {
+class test{
 
-    public test() {
-        setTitle("Rectangle with Divided Squares");
-        setSize(1200, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root","root","root");
+        Statement stmt=con.createStatement();
+        ResultSet s=stmt.executeQuery("Select * from oracle");
 
-        JPanel drawingPanel = new DrawingPanel();
-        getContentPane().add(drawingPanel);
-
-        setVisible(true);
     }
 
-    private class DrawingPanel extends JPanel {
-
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            int rectangleWidth = 800;
-            int rectangleHeight = 50;
-
-            int squareSize = rectangleWidth / 10;
-
-            int startX = 100;
-            int startY = 50;
-
-            // Draw the rectangle
-//            g.drawRect(startX, startY, rectangleWidth, rectangleHeight);
-
-//             Draw the divided squares
-            for (int i = 0; i < 10; i++) {
-                int x = startX + i * squareSize;
-                int y = startY;
-                g.drawRect(x, y, squareSize, squareSize/2);
-                Font f1=new Font("Arial",Font.BOLD,18);
-                g.setFont(f1);
-                g.drawString("Q"+(i+1),x+28,y+30);
-            }
-            for (int i = 0; i < 10; i++) {
-                int x = startX + i * squareSize;
-                int y = startY+40;
-                g.drawRect(x, y, squareSize, squareSize/2);
-                if (i % 2 == 0) {
-                    drawTick(g, x, y, squareSize, squareSize / 2);
-                } else {
-                    drawCross(g, x, y, squareSize-10, squareSize / 2);
-                }
-            }
-        }
-        private void drawTick(Graphics g, int x, int y, int width, int height) {
-            int[] xPoints = {x + width / 4, x + width / 2, x + 3 * width / 4};
-            int[] yPoints = {y + height / 2, y + 3 * height / 4, y + height / 4};
-            g.drawPolyline(xPoints, yPoints, 3);
-        }
-
-        private void drawCross(Graphics g, int x, int y, int width, int height) {
-            g.drawLine(x+10, y, x + width-10, y + height);
-            g.drawLine(x+10, y + height, x + width-10, y);
-        }
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new test());
-    }
 }
