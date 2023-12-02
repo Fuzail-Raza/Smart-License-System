6e18272705fda8cf075a2d4f4425c78865f3a70d
Fuzail Raza <fuzailraza161@gmail.com>
2023-12-02 17:19:17 +0500

    Updated UI LoginForm

--- mongoConnect/AdminLogin.java
+++ mongoConnect/AdminLogin.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,30 +32,56 @@ class Login{
 
         JFrame mainFrame = new JFrame();
 
-        innerPanel=new JPanel(new GridLayout(4,2));
+        innerPanel=new JPanel(null);
 
-        heading=new JLabel("User LogIN");
+        Border etchedBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+
+
+        TitledBorder titledBorder = BorderFactory.createTitledBorder(etchedBorder, "Driving License User LOGIN");
+        titledBorder.setTitleColor(Color.BLACK);
+        Font labelFont = new Font("Arial", Font.BOLD, 28);
+        titledBorder.setTitleJustification(TitledBorder.CENTER);
+        titledBorder.setTitleFont(labelFont);
+
+        innerPanel.setBorder(titledBorder);
+
+        heading=new JLabel("User LOGIN");
+        heading.setHorizontalTextPosition(SwingConstants.CENTER);
+        heading.setFont(labelFont);
         innerPanel.add(heading);
+
         innerPanel.add(new JLabel());
 
+        Font font = new Font("Arial", Font.BOLD, 16);
         userNameLabel=new JLabel("User Name : ");
+        userNameLabel.setFont(font);
         innerPanel.add(userNameLabel);
 
         userName=new JTextField();
         innerPanel.add(userName);
 
         passwordLabel=new JLabel("Password");
+        passwordLabel.setFont(font);
         innerPanel.add(passwordLabel);
 
         password=new JPasswordField();
         innerPanel.add(password);
 
         LogIN=new JButton("LOGIN");
+        LogIN.setFont(font);
+
         innerPanel.add(LogIN);
+
+        LogIN.setBounds (270, 285, 170, 30);
+        userNameLabel.setBounds (190, 150, 130, 30);
+        passwordLabel.setBounds (190, 200, 130, 30);
+        password.setBounds (335, 200, 170, 30);
+        userName.setBounds (335, 150, 170, 30);
+        heading.setBounds (245, 80, 205, 40);
 
         mainFrame.add(innerPanel);
         mainFrame.setVisible(true);
-        mainFrame.setSize(720,600);
+        mainFrame.setSize(720,580);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
